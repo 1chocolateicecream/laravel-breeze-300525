f3b94b20663d1b5bdc35a6ada6c34ba5d3260df2
pipisa <your@email.ex>
2025-05-30 09:53:38 +0300

    ui cool

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,33 +1,34 @@
 <x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <h1>All posts</h1>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-4 p-4">
     <div class="text-gray-900 dark:text-gray-100 mb-2">
         <a href="{{ route('dashboard') }}">Back to dashboard</a><br>
         <a href="{{ route('post.create') }}">Create post</a>
     </div>
+    </div>
     </div><br>
     <ul>
-        @foreach($posts as $post)
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-4 p-4">
-            <li>
-                <div class="text-gray-900 dark:text-gray-100 mb-2">
-                    <h2>Title: {{ $post->title }}</h2>
-                    <p>Content: {{ $post->content }}</p>
-                </div>
-                <div>
-                    <a href="{{ route('post.show', $post) }}">Show</a>
-                    <a href="{{ route('post.edit', $post) }}">Edit</a>
-                    <form action="{{ route('post.destroy', $post) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        
-                        <button>Delete</button>
-                    </form>
-                </div>
-            </li>
+    @foreach($posts as $post)
+        <li class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-4 p-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="text-gray-900 dark:text-gray-100 mb-2">
+            <h2>Title: {{ $post->title }}</h2>
+            <p>Content: {{ $post->content }}</p>
+            </div>
+            <div>
+            <a href="{{ route('post.show', $post) }}">Show</a>
+            <a href="{{ route('post.edit', $post) }}">Edit</a>
+            <form action="{{ route('post.destroy', $post) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button>Delete</button>
+            </form>
+            </div>
         </div>
-        @endforeach
-
-        @include('layouts.footer')
+        </li>
+    @endforeach
     </ul>
 </x-app-layout>
