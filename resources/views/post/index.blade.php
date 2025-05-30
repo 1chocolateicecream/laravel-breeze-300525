45e1407219f229b76fac6ad55feb4672e5f98a04
pipisa <your@email.ex>
2025-05-30 09:20:22 +0300

    breeze

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,11 +1,19 @@
 <x-app-layout>
     <h1>All posts</h1>
-    <a href="{{ route('post.create') }}">Create post</a>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-4 p-4">
+    <div class="text-gray-900 dark:text-gray-100 mb-2">
+        <a href="{{ route('dashboard') }}">Back to dashboard</a><br>
+        <a href="{{ route('post.create') }}">Create post</a>
+    </div>
+    </div><br>
     <ul>
         @foreach($posts as $post)
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg my-4 p-4">
             <li>
-                <h2>Title: {{ $post->title }}</h2>
-                <p>Content: {{ $post->content }}</p>
+                <div class="text-gray-900 dark:text-gray-100 mb-2">
+                    <h2>Title: {{ $post->title }}</h2>
+                    <p>Content: {{ $post->content }}</p>
+                </div>
                 <div>
                     <a href="{{ route('post.show', $post) }}">Show</a>
                     <a href="{{ route('post.edit', $post) }}">Edit</a>
@@ -17,6 +25,7 @@
                     </form>
                 </div>
             </li>
+        </div>
         @endforeach
 
         @include('layouts.footer')
